815166f09938e43b16287d3a0dfa15a882a678b2
Nicollas Lopes <nicollaslopesn@gmail.com>
2019-03-03 19:38:11 -0300

    dns

---
+++ bruteforce/dnsbrute.py
@@ -0,0 +1,26 @@
+import dns.resolver
+import sys
+
+print('>>>>>>>>>>>> MODO DE USAR: python dnsbrute.py <dominio> <wordlist> ')
+
+try:
+    dominio = sys.argv[1]
+    arquivo = sys.argv[2]
+except:
+    print ('Argumentos invalidos!')
+
+try:
+    arquivo = open(arquivo)
+    subDominios = arquivo.read().splitlines()
+except:
+    print('Arquivo nao encontrado...')
+    sys.exit()
+
+for subDominio in subDominios:
+    try:
+        domESub = subDominio + '.' + dominio
+        resultados = dns.resolver.query(domESub, 'a')
+        for resultado in resultados:
+            print("Subdomonio: ", domESub, "| IP: ->", resultado)
+    except:
+        pass
